24fda6cfe72377a887aa7cdbfd4967b63e67f115
Jinyoung Choi <jinyoung5497@gmail.com>
2024-07-25 14:17:04 +1000

    home page image resolution fixes

--- src/component/FeaturedSkill.jsx
+++ src/component/FeaturedSkill.jsx
@@ -26,15 +26,15 @@ export default function FeaturedSkill() {
       <div className="grid grid-cols-3 gap-10 mx-0 lg:mx-10 ml:mx-0 lg:grid-cols-2 xs:gap-5 2xs:gap-2 3xs:flex 3xs:flex-col">
         <div
           onClick={() => openProject(1, "Kanban Task Management App")}
-          className="bg-blue-400 p-4 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black max-h-[500px]"
+          className="bg-blue-400 p-4 md:p-2 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black"
         >
-          <div className="text-white h-1/5 flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
+          <div className="text-white  flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
             Kanban Task Management App
           </div>
           <img
             src={kanban_001}
             alt="kanban_001"
-            className="rounded-xl h-4/5 2xs:rounded-lg mb-7"
+            className="rounded-xl h-full w-full 2xs:rounded-lg mb-7 "
           />
           <div className="flex flex-wrap gap-2">
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
@@ -56,16 +56,16 @@ export default function FeaturedSkill() {
         </div>
 
         <div
-          onClick={() => openProject(4, "Audiophile Ecommerce Website")}
-          className="bg-orange p-4 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black  max-h-[500px]"
+          onClick={() => openProject(4, "Audiophile E-commerce Website")}
+          className="bg-orange p-4 md:p-2 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black"
         >
-          <div className="text-white h-1/5 flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
+          <div className="text-white  flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
             Audiophile E-commerce Website
           </div>
           <img
             src={ecommerce_010}
             alt="ecommerce_010"
-            className="rounded-xl h-4/5 2xs:rounded-lg mb-7"
+            className="rounded-xl h-full w-full 2xs:rounded-lg mb-7 "
           />
           <div className="flex flex-wrap gap-2">
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
@@ -76,9 +76,6 @@ export default function FeaturedSkill() {
             </div>
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
               TypeScript
-            </div>
-            <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
-              React Redux
             </div>
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
               MongoDB
@@ -94,15 +91,15 @@ export default function FeaturedSkill() {
 
         <div
           onClick={() => openProject(2, "Connect Four Game")}
-          className="bg-purple-600 p-4 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black  max-h-[500px]"
+          className="bg-purple-600 p-4 md:p-2 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black"
         >
-          <div className="text-white h-1/5 flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
+          <div className="text-white  flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
             Connect Four Game
           </div>
           <img
             src={connect_001}
             alt="connect_001"
-            className="rounded-xl h-4/5 2xs:rounded-lg mb-7"
+            className="rounded-xl h-full w-full 2xs:rounded-lg mb-7 "
           />
           <div className="flex flex-wrap gap-2">
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
@@ -122,17 +119,17 @@ export default function FeaturedSkill() {
 
         <div
           onClick={() => openProject(3, "Multi Step Form")}
-          className="bg-indigo-600 p-4 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black  max-h-[500px]"
+          className="bg-indigo-600 p-4 md:p-2 rounded-xl flex flex-col items-center justify-center cursor-pointer border-[1px] border-black"
         >
-          <div className="text-white h-1/5 flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
+          <div className="text-white  flex items-center justify-center text-lg font-semibold 2xs:text-md text-center mb-5">
             Multi Step Form
           </div>
           <img
             src={multi_step_001}
             alt="multi_step_001"
-            className="rounded-xl h-4/5 2xs:rounded-lg mb-7"
+            className="rounded-xl h-full w-full 2xs:rounded-lg mb-7 "
           />
-          <div className="flex flex-wrap gap-2 items-center justify-start">
+          <div className="flex flex-wrap gap-2">
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
               React
             </div>
@@ -140,7 +137,7 @@ export default function FeaturedSkill() {
               React Router Dom
             </div>
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
-              useContext React Hook
+              useContext
             </div>
             <div className="bg-gray px-2 py-1 rounded-full text-black border-black border-[1px] text-sm">
               Tailwind CSS
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { NavLink } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { hoverTrue } from '../slices/pageSlice'
-import { FeaturedSkill, Footer } from '.'
+import React from "react";
+import Navbar from "./Navbar";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { hoverTrue } from "../slices/pageSlice";
+import { FeaturedSkill, Footer } from ".";
 
 /**
  * Home extended card?
@@ -12,47 +12,47 @@ import { FeaturedSkill, Footer } from '.'
  */
 
 export default function Home() {
-  const dispatch = useDispatch()
-  const page = useSelector((state) => state.page.value)
+  const dispatch = useDispatch();
+  const page = useSelector((state) => state.page.value);
 
   const hoverFalses = () => {
-    dispatch(hoverTrue())
-    window.scrollTo({ top: 0 })
-  }
+    dispatch(hoverTrue());
+    window.scrollTo({ top: 0 });
+  };
 
   return (
     <>
       <Navbar />
-      <div className='flex items-center justify-center flex-col gap-12 mx-32 m:mx-20 sm:mx-16 2xs:mx-10 3xl:gap-5'>
-        <h1 className='text-[100px] lg:text-[80px] m:text-[70px] xs:text-[55px] 2xs:text-[40px] 4xs:text-[35px] font-bold my-5 bg-gradient-to-r from-blue-500 to-fuchsia-500 text-transparent bg-clip-text text-center xs:my-0'>
+      <div className="flex items-center justify-center flex-col gap-12 mx-32 m:mx-20 sm:mx-16 2xs:mx-10 3xl:gap-5">
+        <h1 className="text-[100px] lg:text-[80px] m:text-[70px] xs:text-[55px] 2xs:text-[40px] 4xs:text-[35px] font-bold my-5 bg-gradient-to-r from-blue-500 to-fuchsia-500 text-transparent bg-clip-text text-center xs:my-0">
           Jinyoung Choi
         </h1>
-        <p className='text-xl text-slate-600 text-center max-w-7xl xs:text-md 2xs:max-w-md'>
+        <p className="text-xl text-slate-600 text-center max-w-7xl xs:text-md 2xs:max-w-md">
           Welcome to my portfolio! I am a passionate frontend developer with a
           keen eye for creating stunning and user-friendly web experiences. Here
           you will find a showcase of my polished projects and prototypes!
         </p>
-        <div className='flex gap-10 mb-10 xs:gap-4 xs:mb-0'>
+        <div className="flex gap-10 mb-10 xs:gap-4 xs:mb-0">
           <NavLink
-            to='/projects'
+            to="/projects"
             onClick={hoverFalses}
-            className='h-12 bg-gradient-to-r from-pink-500 to-violet-500 hover:from-pink-600 hover:to-violet-600 rounded-lg p-5 flex justify-center items-center text-white px-12 font-medium border-[1px] xs:px-4 w-40 xs:w-32 3xs:w-24 3xs:p-4 3xs:px-10 4xs:w-20  xs:text-sm xs:h-3 self-center'
+            className="h-12 bg-gradient-to-r from-pink-500 to-violet-500 hover:from-pink-600 hover:to-violet-600 rounded-lg p-5 flex justify-center items-center text-white px-12 font-medium border-[1px] xs:px-4 w-40 xs:w-32 3xs:w-24 3xs:p-4 3xs:px-10 4xs:w-20  xs:text-sm xs:h-3 self-center"
           >
             Projects
           </NavLink>
           <a
-            href='https://github.com/jinyoung5497'
-            className='h-12 hover:bg-gradient-to-r hover:from-violet-500 hover:to-cyan-500 rounded-lg p-5 flex justify-center items-center hover:text-white px-12 font-medium border-[1px] border-violet-500 xs:px-4 w-40 xs:w-32 3xs:w-24 3xs:p-4 3xs:px-10 4xs:w-20  xs:text-sm xs:h-3 self-center'
+            href="https://github.com/jinyoung5497"
+            className="h-12 hover:bg-gradient-to-r hover:from-violet-500 hover:to-cyan-500 rounded-lg p-5 flex justify-center items-center hover:text-white px-12 font-medium border-[1px] border-violet-500 xs:px-4 w-40 xs:w-32 3xs:w-24 3xs:p-4 3xs:px-10 4xs:w-20  xs:text-sm xs:h-3 self-center"
           >
             Github
           </a>
         </div>
 
-        <div className='flex flex-col items-center gap-10 w-full mb-10'>
+        <div className="flex flex-col items-center gap-10 w-full mb-10">
           <FeaturedSkill />
         </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
